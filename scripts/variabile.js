259585dd0fd32b06754e2e48b56f272ea0025eb6
Draghi <draghicabogdan@yahoo.com>
2023-05-23 18:09:09 +0300

    Added variabile.js

---
+++ scripts/variabile.js
@@ -0,0 +1,72 @@
+/* -------- // Exercițiul 1: -------- */
+console.log("----- Exercițiul 1: -----");
+// RO: Scrieți un program JavaScript care adună două numere și afișează rezultatul în consolă.
+var x = 10;
+var y = 70;
+var suma = x + y;
+console.log(suma); // output: 80
+
+
+/* -------- // Exercițiul 2: -------- */
+console.log("----- Exercițiul 2: -----");
+// RO: Scrieți un program JavaScript care calculează media a trei numere și afișează rezultatul în consolă.
+var a = 30;
+var b = 10;
+var c = 20;
+var medie = (a + b + c) / 3;
+console.log(medie); // output: 20
+
+
+/* -------- // Exercițiul 3: -------- */
+console.log("----- Exercițiul 3: -----");
+// RO: Scrieți un program JavaScript care adaugă 5 la o variabilă numită "x" și afișează rezultatul în consolă.
+var x = 20;
+x = x + 5;
+console.log(x); // output: 25
+
+
+/* -------- // Exercițiul 4: -------- */
+console.log("----- Exercițiul 4: -----");
+// RO: Scrieți un program JavaScript care atribuie valoarea 10 variabilei "y" și apoi adaugă 2 la aceasta utilizând operatorul "+=".
+var y = 10;
+y += 2;
+console.log(y); // output: 12
+
+
+/* -------- // Exercițiul 5: -------- */
+console.log("----- Exercițiul 5: -----");
+// RO: Scrieți un program JavaScript care compară două numere și afișează "A este mai mare decât B" dacă primul număr este mai mare decât al doilea, sau "B este mai mare sau egal cu A" dacă al doilea număr este mai mare sau egal cu primul.
+var A = 100;
+var B = 70;
+var result = A > B;
+console.log(100 > 70); // output: true
+
+
+/* -------- // Exercițiul 6: -------- */
+console.log("----- Exercițiul 6: -----");
+// RO: Scrieți un program JavaScript care verifică dacă o variabilă numită "varsta" este mai mare sau egală cu 18 și afișează un mesaj corespunzător în consolă, în funcție de rezultat.
+var varsta >= 18;
+var varsta = 7;
+ varsta >= 18;
+ console.log(7 >= 18); // output: false
+
+
+ /* -------- // Exercițiul 7: -------- */
+console.log("----- Exercițiul 7: -----");
+// RO: Scrieți un program JavaScript care verifică dacă două condiții sunt adevărate utilizând operatorul "&&" (și logic) și afișează un mesaj corespunzător în consolă, în funcție de rezultat.
+var x = 25;
+var y = 30;
+(x < 40 && y < 50);
+(x > 5 && y < 40);
+console.log; // output: true; true
+
+
+ /* -------- // Exercițiul 8: -------- */
+ console.log("----- Exercițiul 8: -----");
+ // RO: Scrieți un program JavaScript care verifică dacă o condiție este adevărată utilizând operatorul "!" (nu logic) și afișează un mesaj corespunzător în consolă, în funcție de rezultat.
+var x = 10;
+var y = 5;
+!(x < 5);
+console.log; // output: true
+
+
